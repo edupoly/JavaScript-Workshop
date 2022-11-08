018ce9730903b632c3d389894844c51b46b562e8
edupoly <info.edupoly@gmail.com>
2022-11-08 23:10:12 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,4 @@
+import division,{add as addition,mul} from './utils.js'
+console.log(mul(33,4,2))
+console.log(division(10,2))
+console.log(addition(33,13))
---
+++ utils.js
@@ -0,0 +1,9 @@
+export function add(a,b,c=0){
+    return a+b+c;
+}
+export function mul(a,b,c=1){
+    return a*b*c;
+}
+export default function div(a,b){
+    return a/b
+}
